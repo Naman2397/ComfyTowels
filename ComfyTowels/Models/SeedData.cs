886fe133937eb4913782b92caff4cb1b053ea38c
Naman2397 <naman.oye.org23@gmail.com>
2024-02-22 09:45:09 -0500

    Add project files.

---
+++ ComfyTowels/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ComfyTowels.Data;
+using System;
+using System.Linq;
+
+namespace ComfyTowels.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new ComfyTowelsContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<ComfyTowelsContext>>()))
+        {
+            // Look for any towels.
+            if (context.Towels.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Towels.AddRange(
+                new Towels
+                {
+                    TowelType = "Type 1",
+                    Size = "Medium",
+                    Color = "Black",
+                    Price = 7
+                },
+                new Towels
+                {
+                    TowelType = "Type 2 ",
+                    Size = "Small",
+                    Color = "White",
+                    Price = 5
+                },
+                new Towels
+                {
+                    TowelType = "Type 3",
+                    Size = "Large",
+                    Color = "Grey",
+                    Price = 9
+                },
+                new Towels
+                {
+                    TowelType = "Type 4",
+                    Size = "X-Large",
+                    Color = "Yellow",
+                    Price = 10
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ComfyTowels/Models/TowelColorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ComfyTowels.Models;
+
+public class TowelColorViewModel
+{
+    public List<Towels>? Towels { get; set; }
+    public SelectList? TowelType { get; set; }
+    public string? TowelColor { get; set; }
+    public string? SearchString { get; set; }
+}
